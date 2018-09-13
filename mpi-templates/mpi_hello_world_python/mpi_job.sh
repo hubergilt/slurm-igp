afc6ee36c4d889bf947b08cdc35809988de5c73b
Huber Gilt Lopez <hgilt@IGPmaster2.igp.gob.pe>
2018-09-13 12:07:21 -0500

    update mpi python example

--- mpi-templates/mpi_hello_world_python/mpi_job.sh
+++ mpi-templates/mpi_hello_world_python/mpi_job.sh
@@ -4,6 +4,7 @@
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
 #SBATCH --ntasks=24
-srun -n 3  python mpi_hello_world.py "Step-id 0"
-srun -n 5  python mpi_hello_world.py "Step-id 1"
-srun -n 8  python mpi_hello_world.py "Step-id 2"
+srun -n 3  python mpi_hello_world.py "Step-id 0" &
+srun -n 5  python mpi_hello_world.py "Step-id 1" &
+srun -n 8  python mpi_hello_world.py "Step-id 2" &
+wait
